d4d280a6adabfd3ff6a95d0b5d9b15cac1ee2b6b
Hemang Sidapara <hemang@logisticinfotech.co.in>
2023-02-10 14:10:38 +0530

    clipper changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:clipper_demo/clip_shadow_path.dart';
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
@@ -41,29 +39,44 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  // shadow: Shadow(
+  //   blurRadius: 8,
+  //   color: Colors.grey.withOpacity(0.5),
+  //   offset: const Offset(0, 1.5),
+  // ),
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: ClipShadowPath(
-          clipper: CircularNotchedRClipper(30.w),
-          shadow: Shadow(
-            blurRadius: 8,
-            color: Colors.grey.withOpacity(0.5),
-            offset: const Offset(0, 1.5),
-          ),
-          child: Container(
-            height: 30.h,
-            width: 90.w,
-            decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(12)),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: const [
-                Spacer(),
-              ],
+      backgroundColor: Color(0xFFF2F5F8),
+      body: Padding(
+        padding: EdgeInsets.only(top: 14.8.h, left: 5.w, right: 5.w),
+        child: Stack(
+          alignment: Alignment.topCenter,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 9.h),
+              child: ClipShadowPath(
+                clipper: CircularNotchedRClipper(30.w),
+                shadow: Shadow(
+                  blurRadius: 8,
+                  color: Colors.black38,
+                  offset: const Offset(0, 1.5),
+                ),
+                child: Container(
+                  height: 22.h,
+                  width: 90.w,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: Colors.white,
+                  ),
+                ),
+              ),
             ),
-          ),
+            CircleAvatar(
+              radius: 45,
+              backgroundImage: NetworkImage('https://media.istockphoto.com/id/828156368/photo/demo.jpg?s=612x612&w=0&k=20&c=JIREJlrI5vY33-hLNn8vz_GREOoTIFLfSsOSkgYJ_ms='),
+            ),
+          ],
         ),
       ),
     );
@@ -83,17 +96,27 @@ class CircularNotchedRClipper extends CustomClipper<Path> {
     final curve = space / 6;
     final height = halfSpace / 2;
     // final height = halfSpace / 1.4;
-    path.lineTo(halfWidth - halfSpace, 0);
-    // path.cubicTo(halfWidth - 8.w, 0, halfWidth - halfSpace + space / 6, height, halfWidth, height);
-    path.cubicTo(halfWidth - halfSpace, 0, halfWidth - halfSpace + curve, height, halfWidth, height);
+    path.lineTo(halfWidth - halfSpace - 10, 0);
+    path.cubicTo(halfWidth - 5.w, 0, halfWidth - halfSpace + space / 6, height, halfWidth, height);
+    // path.cubicTo(halfWidth - halfSpace, 0, halfWidth - halfSpace + curve, height, halfWidth, height);
 
+    path.cubicTo(halfWidth + 28, height, halfWidth + halfSpace - 18, 0, halfWidth + halfSpace, 0);
     // path.cubicTo(halfWidth, height, halfWidth + halfSpace - curve, height, halfWidth + halfSpace, 0);
-    path.cubicTo(halfWidth, height, halfWidth + halfSpace - curve, height, halfWidth + halfSpace, 0);
 
     path.lineTo(size.width, 0);
     path.lineTo(size.width, size.height);
     path.lineTo(0, size.height);
     path.close();
+
+    // final path = Path()
+    //   ..lineTo(size.width * 0.1, 0)
+    //   ..quadraticBezierTo(size.width * 0.23, size.height / 2000, size.width * 0.25, size.height * 0.2)
+    //   ..quadraticBezierTo(size.width * 0.25, size.height * 0.38, size.width * 0.5, size.height * 0.4)
+    //   ..quadraticBezierTo(size.width * 0.75, size.height * 0.38, size.width * 0.75, size.height * 0.2)
+    //   ..quadraticBezierTo(size.width * 0.77, size.height / 2000, size.width * 0.90, 0)
+    //   ..lineTo(size.width, 0)
+    //   ..lineTo(size.width, size.height)
+    //   ..lineTo(0, size.width);
 
     return path;
   }
